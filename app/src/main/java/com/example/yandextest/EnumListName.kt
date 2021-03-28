ca5beba28f6b350c5236d9beb153a02c80bb9534
admin <ilya.smetanin.2002@mail.ru>
2021-03-29 00:33:34 +0300

    alpha

--- app/src/main/java/com/example/yandextest/EnumListName.kt
+++ app/src/main/java/com/example/yandextest/EnumListName.kt
@@ -7,9 +7,9 @@ enum class EnumListName(val value: String) {
     MY_SYMBOL("<MY_SYMBOL>"),
     MY_SYMBOL2("<MY_SYMBOL2>"),
     STOCKS_TICKERS("https://mboum.com/api/v1/tr/trending?apikey=" + KEY_MBOUM.value),
-    WEB_SOCKET("wss://ws.finnhub.io?token=" + KEY_FINNHUB),
+    WEB_SOCKET("wss://ws.finnhub.io?token=" + KEY_FINNHUB.value),
     QUOTE("https://mboum.com/api/v1/qu/quote/?symbol=<MY_SYMBOL>&apikey=" + KEY_MBOUM.value),
-    SEARCH("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=<MY_SYMBOL>&apikey=" + KEY_ALPHAVANTAGE),
+    SEARCH("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=<MY_SYMBOL>&apikey=" + KEY_ALPHAVANTAGE.value),
     FAVORITES_TICKERS("FAVORITES"),
     STOCKS("STOCKS"),
     FAVORITE("FAVORITE"),
@@ -17,7 +17,7 @@ enum class EnumListName(val value: String) {
     DATA_CHART("https://mboum.com/api/v1/hi/history/?symbol=<MY_SYMBOL>&interval=<MY_SYMBOL2>&diffandsplits=true&apikey=" + KEY_MBOUM.value),
     SUMMARY("https://mboum.com/api/v1/qu/quote/profile/?symbol=<MY_SYMBOL>&apikey=" + KEY_MBOUM.value),
     RECOMMENDATION("https://mboum.com/api/v1/qu/quote/recommendation-trend/?symbol=<MY_SYMBOL>&apikey=" + KEY_MBOUM.value),
-    NEWS_SENTIMENTS("https://finnhub.io/api/v1/news-sentiment?symbol=<MY_SYMBOL>&token=" + KEY_FINNHUB),
+    NEWS_SENTIMENTS("https://finnhub.io/api/v1/news-sentiment?symbol=<MY_SYMBOL>&token=" + KEY_FINNHUB.value),
     NEWS("https://mboum.com/api/v1/ne/news/?symbol=<MY_SYMBOL>&apikey=" + KEY_MBOUM.value)
 
 
